b9a3e6b2f7644575809b3f5dffc49a391422ca34
xElon-Mask <xelonmask@protonmail.com>
2022-04-30 21:01:51 +0200

    add event teacherAdded() + function setTeacher

--- SchoolManageringMark2.sol
+++ SchoolManageringMark2.sol
@@ -21,10 +21,16 @@ Student[] private students;
 mapping (string => mapping (string => address)) teachers;
 
 event studentAdded(string _name, string _class, address _addr);
+event teacherAdded(string _class, string _course, address _addr);
 
 function addStudent(string memory _name, string memory _class, address _addr) public onlyOwner {
     students.push(Student(_name, _class, _addr));
     emit studentAdded(_name, _class, _addr);
+}
+
+function setTeacher(string memory _class, string memory _course, address memory _addr) public onlyOwner {
+    teachers[_class][_course] = _addr;
+    emit teacherAdded(_class, _course, _addr);
 }
 
 }
